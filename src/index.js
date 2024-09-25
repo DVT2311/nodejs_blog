22e515cfdd554970a89ec624216c447d299cfc03
Tam KeyBoard <dinhvantam333@gmail.com>
2024-09-25 10:29:14 +0700

    lint staged

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ const port = 3000;
 
 const route = require('./routes');
 
-    app.use(express.static(path.join(__dirname,
-       'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-      express.urlencoded({
-
+    express.urlencoded({
         extended: true,
     }),
 );
